24ecb8c8eeb6709c87b5fffce1a97c3891a3d806
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2025-02-21 14:51:26 -0600

    basics

---
+++ KotlinBasics/app/src/main/java/com/motawebdev/kotlinbasics/Basics.kt
@@ -0,0 +1,48 @@
+package com.motawebdev.kotlinbasics
+
+fun main() {
+//    val number1 = 1
+//
+//    var myAge: Byte = 35
+//    println(myAge)
+//    myAge = 36
+//
+//    println(myAge)
+//    val pi = 3.14f
+//    println(pi)
+
+//    var age: UShort = 35u
+//    println(age)
+//    val myTrue: Boolean = true
+//    val myFalse: Boolean = false
+//
+//    println(myTrue || myFalse)
+//    println(myTrue && myFalse)
+//    println(!myTrue)
+//    println(!myFalse)
+//    var myChar: Char = 'a'
+//    println(myChar)
+//    myChar = '\u00AE'
+//    println(myChar)
+//    myChar = '\\'
+//    println(myChar)
+//    var name = "Denis"
+//    var firstName = "Denis"
+//    var lastName = "Panjuta"
+//    println(name.lowercase())
+//    println(name.uppercase())
+//    println(name + " Panjuta")
+//    println(firstName + "\n" +lastName)
+    var age = 0
+    age = readln().toInt()
+    if (age >= 21) {
+        println("Now you may drink in the US")
+    } else if (age >= 18) {
+        println("You may vote now")
+    } else if (age >= 16) {
+        println("You may drive now")
+    } else {
+        println("Child")
+    }
+}
+
