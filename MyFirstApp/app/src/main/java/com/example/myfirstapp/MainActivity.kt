e10e63075ea1eff9bfd17826fc355b35db88765b
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2025-02-21 13:48:02 -0600

    v39

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             MyFirstAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Oliver")
+                    Greeting("Oliver X")
                 }
             }
         }
