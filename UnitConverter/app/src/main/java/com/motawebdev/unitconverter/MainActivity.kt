5b97a5ec9313cbb1031fafefc57389be8757d72b
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2025-02-26 12:19:24 -0600

    s5

--- UnitConverter/app/src/main/java/com/motawebdev/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/motawebdev/unitconverter/MainActivity.kt
@@ -4,18 +4,30 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.motawebdev.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,17 +50,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 //        Greeting("Android")
 //        Greeting("Android")
         Text(text = "Unit Converter")
-        OutlinedTextField(value = "", onValueChange = {}, placeholder = { Text("Enter a value") })
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            placeholder = { Text("Enter a value") })
         Row {
-            val context = LocalContext.current
-            Button(onClick = {
-                Toast.makeText(context,"Converting", Toast.LENGTH_SHORT).show() }) {
-                Text(text = "Convert")
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Dropdown Arrow")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text(text = "Millimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
+                }
             }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Dropdown Arrow")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text(text = "Millimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
+                }
+            }
+//            val context = LocalContext.current
+//            Button(onClick = {
+//                Toast.makeText(context,"Converting", Toast.LENGTH_SHORT).show() }) {
+//                Text(text = "Convert")
+//            }
         }
         Text(text = "Result")
     }
